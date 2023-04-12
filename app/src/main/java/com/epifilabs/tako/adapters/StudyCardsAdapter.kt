5503a50ac1958a66b9b960f7b81f8d5bd5de2652
Juan Matute <juanmatute@MacBook-Air-de-Juan.local>
2023-04-12 16:24:49 +0400

    gradient effect

--- app/src/main/java/com/epifilabs/tako/adapters/StudyCardsAdapter.kt
+++ app/src/main/java/com/epifilabs/tako/adapters/StudyCardsAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.epifilabs.tako.R
 import com.epifilabs.tako.models.StudyCardModel
+import com.epifilabs.tako.utlis.GradientConstant
 
 class StudyCardsAdapter(val studyCardsList:ArrayList<StudyCardModel>):RecyclerView.Adapter<StudyCardsAdapter.StudyCardsHolder>() {
 
@@ -18,6 +21,7 @@ class StudyCardsAdapter(val studyCardsList:ArrayList<StudyCardModel>):RecyclerVi
         val learningLanguage = item.findViewById<TextView>(R.id.TvLearningLanguage)
         val mainLanguage = item.findViewById<TextView>(R.id.TvMainLanguage)
         val pronunciation = item.findViewById<TextView>(R.id.TvPronunciation)
+        val cardView = item.findViewById<RelativeLayout>(R.id.RlCellWord)
 
     }
 
@@ -38,6 +42,7 @@ class StudyCardsAdapter(val studyCardsList:ArrayList<StudyCardModel>):RecyclerVi
         holder.learningLanguage.text = currentItem.learningLanguage
         holder.mainLanguage.text = currentItem.mainLanguage
         holder.pronunciation.text = currentItem.pronunciation
+        holder.cardView.background   =   GradientConstant.goldGradient(context)
 
 
 
